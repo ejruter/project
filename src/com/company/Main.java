603a461c76165e812a867c9a13ca8458b801dc4e
Eli Ruter <eliruter99@gmail.com>
2018-10-13 17:25:37 -0500

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte myByteValue = 50;
+
+        short myShortValue = 100;
+
+        int myIntValue = 2000;
+
+        long myLongValue = (5000) + (10*myByteValue) + (myShortValue) + (myIntValue);
+        System.out.println(myLongValue);
+        
+    }
+}
